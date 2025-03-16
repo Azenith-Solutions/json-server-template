74008211aef0d4bd292fef236d4cca3210eae2c5
kauansfr <kauan.franca@sptech.school>
2025-03-16 16:19:14 -0300

    [FEAT] :: including javascript utils

--- script.js
+++ script.js
@@ -2,18 +2,24 @@ const form = document.querySelector('.login-form');
 form.addEventListener('submit', async (event) => {
     event.preventDefault();
 
-    try {
-        const email = document.getElementById('email_input').value;
-        const password = document.getElementById('password_input').value;
+    const email = document.getElementById('email_input').value;
+    const password = document.getElementById('password_input').value;
 
-        const user = await userAuthentication(email, password);
-        
-        if (user) {
-            window.location.href = "logado.html";
-        }
-    } catch (error) {
-        console.error("Erro ao autenticar" + error);
-    }
+    if (!fieldValidations(email, password)) {
+        alert();
+        return;
+    };
+
+    // try {
+
+    //     const user = await userAuthentication(email, password);
+
+    //     if (user) {
+    //         window.location.href = "logado.html";
+    //     }
+    // } catch (error) {
+    //     console.error("Erro ao autenticar" + error);
+    // }
 });
 
 async function userAuthentication(email, password) {
@@ -25,4 +31,22 @@ async function userAuthentication(email, password) {
     }
 
     return responseJson.find(user => user.email === email && user.password === password);
+}
+
+function fieldValidations(email, password) {
+    let passed;
+    let errorMessage;
+    
+    if (email.trim() === '' || password.trim() === '') {
+        return alert("Preencha os campos email e/ou senha devidamente");
+    }
+
+    if (!email.includes('@') || !email.includes('.')) {
+        return alert(`Inclua "@" e "." no campo email`);
+    }
+
+    !password.length && (errorMessage = "A senha deve ter no mínimo 12 caracteres", passed = false);
+
+    const hasUpperCase = /[A-Z]/.test(password);
+    !hasUpperCase && alert("Inclua pelo menos uma letra maiúscula na senha");
 }
